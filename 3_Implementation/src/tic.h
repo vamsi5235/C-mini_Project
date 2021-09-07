7e8ecd323ea79b1eb87ab268322a9a3d18e53a36
Vamsikrishna_bollam <43500438+vamsi5235@users.noreply.github.com>
2021-09-07 15:58:05 +0530

    Update tic.h

--- 3_Implementation/src/tic.h
+++ 3_Implementation/src/tic.h
@@ -1,3 +1,6 @@
+#ifndef TIC_H
+#define TIC_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -11,3 +14,5 @@ static void place();      // used by the computer to place O
 static void placey(int);  // used in Double Player mode by the 2nd player to
                           // place the position of O
 int checkwin();  // checks everytime when a player or computer places 'X' or 'O'
+
+#endif
