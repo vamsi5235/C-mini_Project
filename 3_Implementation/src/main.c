f963cad3088561d9cbb0a79940c64410c528a553
Vamsikrishna_bollam <43500438+vamsi5235@users.noreply.github.com>
2021-09-07 15:47:27 +0530

    Update main.c

--- 3_Implementation/src/main.c
+++ 3_Implementation/src/main.c
@@ -1,16 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <string.h>
-
-// Functions Declarations
-static void singlemode();
-static void doublemode();
-static void placex(int);  // used for placing position of X by the 1st player
-static void place();      // used by the computer to place O
-static void placey(int);  // used in Double Player mode by the 2nd player to
-                          // place the position of O
-int checkwin();  // checks everytime when a player or computer places 'X' or 'O'
 
 /** Tic-Tac-Toe table, so basically we are using variable 'game_table' as the table(size:3X3) and
  * updating it regularly
