32ee439b5c9eb962b82664607646bb6abfd7781f
Vamsikrishna_bollam <43500438+vamsi5235@users.noreply.github.com>
2021-09-07 12:25:38 +0530

    Create tic.h

---
+++ 3_Implementation/inc/tic.h
@@ -0,0 +1,22 @@
+#ifndef __TIC_H__
+#define __TIC_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+static void singlemode();
+
+static void doublemode();
+
+static void placex(int); // used for placing position of X by the 1st player
+
+static void place(); //used by the computer to place O
+
+static void placey(int);  // used in Double Player mode by the 2nd player to
+                          // place the position of O
+
+int checkwin();  // checks everytime when a player or computer places 'X' or 'O'
+
+#endif  /* #define __TIC_H__ */
